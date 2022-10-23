5111da9e60abd213ebcc243a4e6f3c3b69cd525c
aviehu <aviehu@gmail.com>
2022-10-23 21:23:36 +0300

    osc

--- robot/src/utils/runPhase.js
+++ robot/src/utils/runPhase.js
@@ -1,3 +1,8 @@
-export default function runPhase(percentageTable, states) {
+import { Client } from 'node-osc';
 
+export default function runPhase(percentageTable, states) {
+    const client = new Client('10.0.0.3', 8888);
+    client.send('/posture', 2300, -1, -1, -1, () => {
+        client.close();
+    });
 }

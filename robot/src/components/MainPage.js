f08ce5e9bf1ac84ccc95e509979fef1211c91239
aviehu <aviehu@gmail.com>
2022-10-23 23:06:45 +0300

    maybe

--- robot/src/components/MainPage.js
+++ robot/src/components/MainPage.js
@@ -3,18 +3,10 @@ import ContainerLayout from "./ContainerLayout";
 import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
 import {useNavigate} from "react-router-dom";
-import percentage from "../utils/percentage"
-import getStates from "../utils/getStates";
 import runPhase from "../utils/runPhase";
 
 export default function MainPage() {
     const navigate = useNavigate();
-
-    async function startPhase(phase) {
-        const percentageTable = await percentage()
-        const states = await getStates(phase)
-        runPhase(percentageTable, states)
-    }
 
     return (
         <ContainerLayout>
@@ -28,13 +20,13 @@ export default function MainPage() {
             <Stack spacing={5} alignItems={"center"}>
                 <div></div>
                 <Stack direction={"row"}>
-                    <Button onClick={() => startPhase(1)}> Start phase 1 </Button>
+                    <Button onClick={() => runPhase(1)}> Start phase 1 </Button>
                     <IconButton onClick={() => navigate('/editphase/1')}>
                         <EditIcon/>
                     </IconButton>
                 </Stack>
                 <Stack direction={"row"}>
-                    <Button onClick={() => startPhase(2)}> Start phase 2 </Button>
+                    <Button onClick={() => runPhase(2)}> Start phase 2 </Button>
                     <IconButton onClick={() => navigate('/editphase/2')}>
                         <EditIcon/>
                     </IconButton>
--- robot/src/utils/runPhase.js
+++ robot/src/utils/runPhase.js
@@ -1,8 +1,5 @@
-import { Client } from 'node-osc';
-
-export default function runPhase(percentageTable, states) {
-    const client = new Client('10.0.0.3', 8888);
-    client.send('/posture', 2300, -1, -1, -1, () => {
-        client.close();
-    });
+export default async function runPhase(phase) {
+    const response = await fetch(`http://localhost:3001/startphase/${phase}`)
+    const states = await response.json()
+    return states
 }
